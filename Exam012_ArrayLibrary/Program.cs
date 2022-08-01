0d0051e1495f22b784a591eefb5dd67ac26b3dfe
Yuliya Bilera <ulya.tch2010@yandex.ru>
2022-08-01 12:34:12 +0300

    Exam012_ArrayLibrary

---
+++ Exam012_ArrayLibrary/Program.cs
@@ -0,0 +1,51 @@
+﻿void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        //index=index + 1;
+        index ++;
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while(position < count)
+    {
+        Console.WriteLine(col [position]); //значение текущего элемента
+        position++;
+    }
+}
+
+int  indexOf(int[] collection, int find) //будет приходить массив collection и элемент find
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1; //если элемента нет выводит -1 (такой элемент не найден)
+    while(index < count)
+    {
+        if(collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+}
+
+int [] array = new int [10];
+
+FillArray(array); //заполняет массив
+array[4] = 4;
+array[6] = 4;
+
+PrintArray(array); //распечатывает массив
+Console.WriteLine();
+
+int pos = indexOf(array, 444);
+Console.WriteLine(pos);
